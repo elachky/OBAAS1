0079fea74638673d56549444ed50c90f00bae8a1
radid youssef <radid.yoyo@gmail.com>
2021-02-02 17:20:04 +0100

    02/02/2021

--- src/test/java/com/selenium/test/ToRegistration.java
+++ src/test/java/com/selenium/test/ToRegistration.java
@@ -13,38 +13,51 @@ public class ToRegistration {
 	    WebDriver driver = new ChromeDriver();
 	    
 
-	    driver.get("http://localhost:3360/");
+	    driver.get("http://localhost:8888/");
 
 	    WebElement linkReg = driver.findElement(By.id("toregistration"));
 	    linkReg.click();
-	    
+	    //les champs
 	    WebElement firstname = driver.findElement(By.id("firstName"));
 	    WebElement lastname = driver.findElement(By.id("lastName"));
-	    WebElement email = driver.findElement(By.id("email"));
+	    WebElement username = driver.findElement(By.id("username"));
 	    WebElement password = driver.findElement(By.id("password"));
+	    WebElement confirme = driver.findElement(By.id("confirm_password"));
+	    WebElement phone = driver.findElement(By.id("phone"));
+	    WebElement address = driver.findElement(By.id("address"));
 	    WebElement market = driver.findElement(By.id("marketType"));
+	    //button register
 	    WebElement register = driver.findElement(By.id("register"));
 	    
 	    firstname.sendKeys("samir");
 	    lastname.sendKeys("rachidi");
-	    email.sendKeys("radid@gmail.com");
+	    username.sendKeys("samir_rachidi");
 	    password.sendKeys("123456789");
-	    
+	    confirme.sendKeys("123456789");
+	    phone.sendKeys("0666033036");
+	    address.sendKeys("lalla yacout n°70 rue n°162");
+
 	    register.click();
 	    
-	    WebElement LoginHere = driver.findElement(By.id("loginhere"));
-	    LoginHere.click();
+	    /*WebElement LoginHere = driver.findElement(By.id("loginhere"));
+	    LoginHere.click();*/
+	    
+	    driver.get("http://localhost:8888/login");
 	    
 	    WebElement LoginUsername = driver.findElement(By.id("username"));
 	    WebElement LoginPassword = driver.findElement(By.id("password"));
 	    WebElement submitLogin = driver.findElement(By.id("login-submit"));
 	    
-	    LoginUsername.sendKeys("radid@gmail.com");
+	    LoginUsername.sendKeys("samir_rachidi");
 	    LoginPassword.sendKeys("123456789");
 	    submitLogin.click();
 	    
-	    Thread.sleep(5000);
-	    driver.quit();
+	    driver.get("http://localhost:8888/login");
+	    
+	    
+	    
+	    //Thread.sleep(50000);
+	    //driver.quit();
 	    
 	}
 
