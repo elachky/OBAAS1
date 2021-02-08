91246fba9e66c3decf7606d963fd42aba3d455a6
elachky <46869037+elachky@users.noreply.github.com>
2021-02-08 21:51:47 +0100

    testes

--- src/test/java/com/example/demo/ObaasApplicationTests.java
+++ src/test/java/com/example/demo/ObaasApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import net.javaguides.springboot.OBAASApplication;
 
 @SpringBootTest(classes = OBAASApplication.class)
 class ObaasApplicationTests {
